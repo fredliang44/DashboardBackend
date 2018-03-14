f0e6d6e5a8634ba643934c157117201fd03914d3
梁志博 <me@fredliang.cn>
2018-03-14 13:14:48 +0800

    Update mail settings

--- mail/mail.go
+++ mail/mail.go
@@ -9,12 +9,16 @@ import (
 
 // Test is func to test smtp mails is reachable or not
 func Test(addr string) {
+	send("Test Sender", "Test  Subject", "info@fredliang.cn", "info@fredliang.cn", "Testmail")
+}
 
+// send func play a key role in mail pkg
+func send(FromName, Subject, ToName, To, Content string) {
 	m := gomail.NewMessage()
-	m.SetHeader("From", "Test Mail Sender <"+utils.AppConfig.Mail.Username+">")
-	m.SetHeader("Subject", "Test Mail")
-	m.SetAddressHeader("Cc", addr, "Test Mail Receiver")
-	m.SetBody("text/html", "Test Mail!")
+	m.SetHeader("From", FromName+" <"+utils.AppConfig.Mail.Username+">")
+	m.SetHeader("Subject", Subject)
+	m.SetAddressHeader("Cc", To, ToName)
+	m.SetBody("text/html", Content)
 
 	d := gomail.NewDialer(utils.AppConfig.Mail.Server, utils.AppConfig.Mail.Port, utils.AppConfig.Mail.Username, utils.AppConfig.Mail.Password)
 	d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
